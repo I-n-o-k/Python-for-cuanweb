a734ffc5cec681a63fabf9776be69e9ec247d778
Inok <75870644+I-n-o-k@users.noreply.github.com>
2024-09-14 20:42:05 +0700

    Voucher Three: Generate 33 multiple serial number
    09 3GB 7D (VMINI 2)
    06 5GB 7D (V5GB7D)
    09 AON 2,5GB (VAON 1)
    09 AON 6GB (VAON 6)

--- voucherthree.py
+++ voucherthree.py
@@ -1,7 +1,10 @@
 # Define the starting number and the number of rows for each suffix
-start = 310130886951
+start = 310115572767
 suffixes = [
-    (',V1P5GB1D', 150),
+    (',VMINI2', 9),
+    (',V5GB7D', 6),
+    (',VAON1', 9),
+    (',VAON6', 9)
 ]
 
 # Initialize a list to store the results
