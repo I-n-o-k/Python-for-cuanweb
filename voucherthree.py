d261658ead096b172cc2db8e154adc81788cb68a
Inok <75870644+I-n-o-k@users.noreply.github.com>
2025-01-07 16:54:27 +0700

    Update voucherthree.py

--- voucherthree.py
+++ voucherthree.py
@@ -1,7 +1,7 @@
 # Define the starting number and the number of rows for each suffix
-start = 310150275351
+start = 310150275456
 suffixes = [
-    (',V1P5GB1D', 60),
+    (',V1P5GB1D', 45),
 ]
 
 # Initialize a list to store the results
